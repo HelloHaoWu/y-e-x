c76d4a3fe39f46ed3fc0e2352a794911cc034fce
supredu <1020665094@qq.com>
2023-04-12 18:39:58 +0800

    add ownable

--- contracts/core/A.sol
+++ contracts/core/A.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../interfaces/IMintable.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 contract A is ERC20("A", "A"), IMintable ,Ownable {
     // 这个币就默认成随便增发就好了，方便开发者给自己打钱
     function mint(address _account, uint256 _amount) public onlyOwner {
