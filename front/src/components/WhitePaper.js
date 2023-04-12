4c3d5bb149c6b830ad983bd624f780fefa47e13f
0xXiaoChen <1255552127@qq.com>
2023-04-12 19:33:55 +0800

    修改react-pdf的webpack

--- front/src/components/WhitePaper.js
+++ front/src/components/WhitePaper.js
@@ -1,6 +1,6 @@
 //@xiaochen
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../assets/pdf/whitepaper.pdf";
 
